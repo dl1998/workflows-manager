79c023b4e23380decc9a12d9a464a085a8228fc6
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2024-11-10 18:33:31 +0100

    Remove redundant pass statements

--- src/workflows_manager/exceptions.py
+++ src/workflows_manager/exceptions.py
@@ -6,24 +6,20 @@ class InvalidConfiguration(Exception):
     """
     Exception raised when configuration is invalid.
     """
-    pass
 
 
 class MissingParameter(InvalidConfiguration):
     """
     Exception raised when parameter is missing.
     """
-    pass
 
 
 class MissingStep(Exception):
     """
     Exception raised when step from configuration is not registered.
     """
-    pass
 
 class UnknownOption(Exception):
     """
     Exception raised when unknown option is passed.
     """
-    pass
